8db9d631c388c209ed1504a0c4bdff4d5458f55f
OnlineTanmoy <yan.tanmoy88@gmail.com>
2021-03-20 22:03:34 -0700

    alexa project

--- project-jarvis/main.py
+++ project-jarvis/main.py
@@ -1,30 +1,53 @@
 import speech_recognition as sr 
 import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice',voices[1].id)
+engine.setProperty('voice', voices[1].id)
 
 def talk(text):
-    engine.say(text)
-    engine.runAndWait()
+	engine.say(text)
+	engine.runAndWait()
 
 def take_command():
-    try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'alexa' in command:
-                print(command)
-    except:
-        pass
-    return command
+	try: 
+		with sr.Microphone() as source:
+			print('listening...')
+			voice = listener.listen(source)
+			command = listener.recognize_google(voice)
+			command = command.lower()
+			if 'alexa' in command:
+				command = command.replace('alexa', '')
+				print(command)
+	except:
+		pass
+	return command
 
 def run_alexa():
-    command = take_command()
-    if 'play' in command:
-        talk('playing')
-        print('playing')
+	command = take_command()
+	print(command)
+	if 'play' in command:
+		song = command.replace('play', '')
+		talk('playing ' + song)
+		pywhatkit.playonyt(song)
+	elif 'time' in command:
+		time = datetime.datetime.now().strftime('%I:%M %p')
+		print(time)
+		talk('Current time is ' + time)
+	elif 'tell me about' in command:
+		person = command.replace('tell me about', '')
+		info = wikipedia.summary(person, 1)
+		print(info)
+		talk(info)
+	elif 'joke' in command:
+		talk(pyjokes.get_joke())
+	else:
+		talk('Please say it again')
+
+
+run_alexa()
